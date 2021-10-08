a4c337454c0fbc20ed98c0de787ee5ec6800d621
ijc3093 <49662848+ijc3093@users.noreply.github.com>
2021-10-07 22:51:04 -0400

    updated

--- add.php
+++ add.php
@@ -147,7 +147,7 @@
             <h1 class="h2">Dashboard</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
-                <a href="add.php" class="btn btn-sm btn-outline-secondary">Add</a>
+                <button class="btn btn-sm btn-outline-secondary">Add</button>
                 <button class="btn btn-sm btn-outline-secondary">Share</button>
                 <button class="btn btn-sm btn-outline-secondary">Export</button>
               </div>
@@ -158,56 +158,72 @@
             </div>
           </div>
 
-          <div class="table-responsive">
-            <table class="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th>Employment ID</th>
-                  <th>Name</th>
-                  <th>Education</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <?php
-            // This is for connecting SQL SERVER Management Studio from PHP
-                $serverName = "tcp:servermuseai.database.windows.net,1433"; //serverName
-                //Since UID PWD are not specified in the $connectionInfo array,
-                //The connection will attempted using Windows Authentication.
-                $connectionOptions = array(
-                    "Database" => "museaiDB",
-                    "Uid" => "admintest",
-                    "PWD" => "Merciful#100"
-                );
 
-                $conn = sqlsrv_connect($serverName, $connectionOptions);
-                if($conn){
-                    echo "Connection established. <br/>";
-                }else{
-                    echo "Connection could not be established. <br/>";
-                    die(print_r(sqlsrv_errors(), true));
-                }
-                $conn = sqlsrv_connect($serverName, $connectionOptions);
+            <form method="post" action="?action=add" enctype="multipart/form-data" >  
+            Emp Id <input type="text" name="t_emp_id" id="t_emp_id"/></br>  
+            Name <input type="text" name="t_name" id="t_name"/></br>  
+            Education <input type="text" name="t_education" id="t_education"/></br>  
+            E-mail address <input type="text" name="t_email" id="t_email"/></br>  
+            <input type="submit" name="submit" value="Submit" />  
+            </form>  
+            <?php 
+
+            // This is for connecting SQL SERVER Management Studio from PHP
+            $serverName = "tcp:servermuseai.database.windows.net,1433"; //serverName\instanceName
+            //Since UID PWD are not specified in the $connectionInfo array,
+            //The connection will attempted using Windows Authentication.
+            $connectionOptions = array(
+                "Database" => "museaiDB",
+                "Uid" => "admintest",
+                "PWD" => "Merciful#100"
+            );
+
+            $conn = sqlsrv_connect($serverName, $connectionOptions);
+            if($conn){
+                echo "Connection established. <br/>";
+            }else{
+                echo "Connection could not be established. <br/>";
+                die(print_r(sqlsrv_errors(), true));
+            }
+            $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+
+            if ($conn === false)  
+                {  
+                die(print_r(sqlsrv_errors() , true));  
+                }  
+                if (isset($_GET['action']))  
+                {  
+                if ($_GET['action'] == 'add')  
+                    {  
+                    /*Insert data.*/  
+                    $insertSql = "INSERT INTO museai.emp2 (emp_id,name,education,email)   
+            VALUES (?,?,?,?)";  
+                    $params = array(&$_POST['t_emp_id'], &$_POST['t_name'], &$_POST['t_education'], &$_POST['t_email']  
+                    );  
+                    $stmt = sqlsrv_query($conn, $insertSql, $params);  
+                    if ($stmt === false)  
+                        {  
+                        /*Handle the case of a duplicte e-mail address.*/  
+                        $errors = sqlsrv_errors();  
+                        if ($errors[0]['code'] == 2601)  
+                            {  
+                            echo "The e-mail address you entered has already been used.</br>";  
+                            }  
             
-                $sql = "SELECT * FROM museai.emp2"; 
-                $stmt = sqlsrv_query($conn, $sql); 
-                if($stmt === false){ 
-                    die(print_r(sqlsrv_errors(), true)); 
+                        /*Die if other errors occurred.*/  
+                        else  
+                            {  
+                            die(print_r($errors, true));  
+                            }  
+                        }  
+                    else  
+                        {  
+                        echo "Registration complete.</br>";  
+                        }  
+                    }  
                 } 
-                
-                if(sqlsrv_has_rows($stmt)){ 
-                    echo"<tbody>";
-                    while($row = sqlsrv_fetch_array($stmt)){ 
-                        // print("<tr><td>".$row['earningsID']."</td>");
-                        print("<tr><td>".$row['emp_id']."</td>"); 
-                        print("<td>".$row['name']."</td>"); 
-                        print("<td>".$row['education']."</td>"); 
-                        print("<td>".$row['email']."</td></tr>"); 
-                    } 
-                    print("</tbody>"); 
-                }
             ?>
-            </table>
-          </div>
         </main>
       </div>
     </div>
